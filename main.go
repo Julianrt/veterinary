1a2dc96a9c910f9d05bbf6a41c55172d2eb7137e
Julian Ruiz Tequida <jnrt96@gmail.com>
2018-04-22 22:09:21 -0700

    inicio del proyecto de una veterinaria para un proyecto escolar usando beego, web framework de go

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "github.com/Julianrt/beego/veterinaria/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
---
+++ routers/router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"github.com/Julianrt/beego/veterinaria/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
